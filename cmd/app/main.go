3ac9cd9c00eb3cf581e2b5ed3c217cefe1862c17
Sergey Ryazanov <majo78@mail.ru>
2024-07-08 08:55:02 +0300

    feat(main): Implement scheduled backup functionality using rsync and cron
    - Added a new function `runBackup` in the main package to handle the backup process. This function reads paths from the specified `paths.txt` file and uses `rsync` to synchronize files to the designated backup directory.
    
    - Integrated the `cron` library to schedule daily backups. The `runBackup` function is configured to run every day at midnight, ensuring regular backups of important directories.
    
    - Setup the `cron` scheduler in the `main` function to start the scheduled task and keep the application running indefinitely. This setup uses the configuration loaded from `config/config.json` to determine the source paths and backup directory.
    
    - Handled errors effectively throughout the backup process, including file reading errors, `rsync` execution errors, and scanner errors, to ensure robust operation.
    
    This commit establishes a reliable, automated backup system within our application, leveraging `rsync` for efficient data synchronization and `cron` for regular scheduling, thus enhancing data safety and operational continuity.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"backupergo/internal/config"
-	"backupergo/internal/service"
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/robfig/cron/v3"
 )
+
+func runBackup(pathsFile, backupDir string) {
+	file, err := os.Open(pathsFile)
+	if err != nil {
+		fmt.Println("Error opening paths file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		path := scanner.Text()
+		base := filepath.Base(path)
+		dest := filepath.Join(backupDir, base)
+
+		// Вызов rsync для каждого пути
+		cmd := exec.Command("rsync", "-avz", path, dest)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to rsync %s: %v\n", base, err)
+		} else {
+			fmt.Printf("Successfully backed up %s to %s\n", base, dest)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading paths file:", err)
+	}
+}
 
 func main() {
 	cfg, err := config.LoadConfig("config/config.json")
 	if err != nil {
-		fmt.Printf("Failed to load config: %v\n", err)
+		fmt.Println("Failed to load configuration:", err)
 		return
 	}
 
-	err = service.ManageDirectories(cfg)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
+	c := cron.New()
+	c.AddFunc("@daily", func() { runBackup(cfg.PathsFile, cfg.BackUpDir) })
+	c.Start()
+
+	// Блокирует основной поток, пока не будет завершен
+	select {}
 }
